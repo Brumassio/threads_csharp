0e83426dee65b75ee28b1a2970f78288c0f884c7
Brumassio <dibrunspeed@gmail.com>
2022-10-27 19:13:49 -0300

    Talvez mudar a lógica das funções das threads (ta mais lento que o sequencial :p)

--- Conta.cs
+++ Conta.cs
@@ -11,62 +11,153 @@ namespace Arq2_Thread
             this.array1 = array1; 
             this.array2 = array2;
         }
-        public void soma() {
-            try
-            {
-                System.Console.WriteLine($"Thread: {Thread.CurrentThread.Name}");
-                for (int i=0;i<this.array1.GetLength(0);i++) {
-                    for (int j=0;j<this.array2.GetLength(1);j++) {
-                        System.Console.WriteLine($"Resultado da soma da pos [{i}][{j}]: {this.array1[i,j]+this.array2[i,j]}");
+
+        public int[,]? multiplicaSeq(){
+            int l1 = this.array1.GetLength(0); //linha do array1
+            int c1 = this.array1.GetLength(1); //coluna do array1
+            int l2 = this.array2.GetLength(0); //linha do array2
+            int c2 = this.array2.GetLength(1); //coluna do array2
+
+            int res = 0;
+            int[,] matriz = new int[l1,c2];
+
+            for(int i = 0;i< l1;i++){
+                for (int j = 0; j < c2; j++){
+                    res = 0;
+                    for(int m =0;m<c1; m++)
+                    {
+                        res += this.array1[i,m] * this.array2[m, j];
                     }
-                    Thread.Sleep(random.Next(1));   
-                }                
+                    System.Console.WriteLine($"Multiplicação resulta em : {res}");
+                    matriz[i,j] = res;
+                }
             }
-            catch (System.NullReferenceException e) 
+            return matriz;
+        }
+
+        public int[,] transpostaSeq(){
+            int l1 = this.array1.GetLength(0);
+            int c1 = this.array1.GetLength(1); 
+            // int[,] matriz = new int[l1,c1];
+            int[,] res = new int[c1,l1];
+
+           // System.Console.WriteLine($"Matriz: {array1[i,j]}");
+
+            for(int i = 0;i<l1;i++)
             {
-                System.Console.WriteLine(e.Message);
-                throw;
+                for(int j = 0; j<c1;j++)
+                {
+                    res[j,i] =  this.array1[i,j];
+                    System.Console.WriteLine($"Matriz Transposta : {res}");
+                }
             }
+            return res;
+        }
+    
+        public int[,] subtracaoSeq(){
+            int [,]vet = new int [this.array1.GetLength(0),this.array1.GetLength(1)];
+            for(int i = 0;i< array1.GetLength(0);i++){
+                for (int j = 0; j < array1.GetLength(0); j++){
+                    System.Console.WriteLine($"Subtração os valores {this.array1[i,j]} e {this.array2[i,j]} tem como resultado: {this.array1[i,j]-this.array2[i,j]}");
+                    vet[i,j] = this.array1[i,j]-this.array2[i,j];
+                }
+            }
+            return vet;
+        }
+
+        public int[,] somaSeq(){
+            int [,]vet = new int[this.array1.GetLength(0),this.array1.GetLength(1)];
+            for (int i = 0; i < array1.GetLength(0); i++){
+                for (int j = 0; j < array1.GetLength(1); j++){
+                    System.Console.WriteLine($"Somando os valores {array1[i,j]} e {array2[i,j]} tem como resultado: {array1[i,j]+array2[i,j]}");
+                    vet[i,j] = array1[i,j]+array2[i,j];
+                }
+            }
+            return vet;
+        }
+
+        // A seguir ficam as funções utlizadas para as threads
+        public void soma1() {
+            // System.Console.WriteLine($"Thread: {Thread.CurrentThread.Name}");
+            for (int i=0;i<this.array1.GetLength(0)/2;i++) {
+                for (int j=0;j<this.array2.GetLength(1);j++) {
+                    System.Console.WriteLine($"Resultado da soma da pos [{i}][{j}]: {this.array1[i,j]+this.array2[i,j]}");
+                }
+                Thread.Sleep(0);   
+            }                
+    
 
         }
 
-        public void subtracao() {
-            try
-            {
-                System.Console.WriteLine($"Thread: {Thread.CurrentThread.Name}");
-                for (int i=0;i<this.array1.GetLength(0);i++) {
-                    for (int j=0;j<this.array2.GetLength(1);j++) {
-                        System.Console.WriteLine($"Resultado da subtração da pos [{i}][{j}]: {this.array1[i,j]-this.array2[i,j]}");
-                    }
-                    Thread.Sleep(random.Next(1));   
-                }            
-            }
-            catch (System.NullReferenceException e)
-            {
-                System.Console.WriteLine(e.Message);
-                throw;
-            }    
-
+        public void soma2(){
+            for (int i= this.array1.GetLength(0)/2;i<this.array1.GetLength(0);i++) {
+                for (int j=0;j<this.array2.GetLength(1);j++) {
+                    System.Console.WriteLine($"Resultado da soma da pos [{i}][{j}]: {this.array1[i,j]+this.array2[i,j]}");
+                }
+                Thread.Sleep(0);   
+            }             
         }
 
-        public void multiplicacao() {
-            try
-            {
-                System.Console.WriteLine($"Thread: {Thread.CurrentThread.Name}");
-                for (int i=0;i<this.array1.GetLength(0);i++) {
-                    for (int j=0;j<this.array2.GetLength(1);j++) {
-                        System.Console.WriteLine($"Resultado da multiplicação da pos [{i}][{j}]: {this.array1[i,j]*this.array2[i,j]}");
-                    }
-                    Thread.Sleep(random.Next(1));   
-                }            
-            }
-            catch (System.NullReferenceException e)
-            {
-                System.Console.WriteLine(e.Message);
-                throw;
-            }
+        public void subtracao1() {
+            for (int i=0;i<this.array1.GetLength(0)/2;i++) {
+                for (int j=0;j<this.array2.GetLength(1);j++) {
+                    System.Console.WriteLine($"Resultado da subtração da pos [{i}][{j}]: {this.array1[i,j]-this.array2[i,j]}");
+                }
+                Thread.Sleep(0);   
+            }            
+        }
+        public void subtracao2() {
+            for (int i=this.array1.GetLength(0)/2 ;i<this.array1.GetLength(0) ;i++) {
+                for (int j=0;j<this.array2.GetLength(1);j++) {
+                    System.Console.WriteLine($"Resultado da subtração da pos [{i}][{j}]: {this.array1[i,j]-this.array2[i,j]}");
+                }
+                Thread.Sleep(0);   
+            }            
+        }
+        public void multiplicacao1() {
+            int l1 = this.array1.GetLength(0)/2 ;//linha do array1
+            int c1 = this.array1.GetLength(1); //coluna do array1
+            int l2 = this.array2.GetLength(0)/2; //linha do array2
+            int c2 = this.array2.GetLength(1); //coluna do array2
 
-        }                        
+            int res = 0;
+            int[,] matriz = new int[l1,c2];
+
+            for(int i = 0;i< l1;i++){
+                for (int j = 0; j < c2; j++){
+                    res = 0;
+                    for(int m =0;m<c1; m++)
+                    {
+                        res += this.array1[i,m] * this.array2[m, j];
+                    }
+                    System.Console.WriteLine($"Multiplicação resulta em : {res}");
+                    matriz[i,j] = res;
+                }
+            }         
+
+        }
+        public void multiplicacao2() {
+            int l1 = this.array1.GetLength(0);//linha do array1
+            int c1 = this.array1.GetLength(1); //coluna do array1
+            int l2 = this.array2.GetLength(0); //linha do array2
+            int c2 = this.array2.GetLength(1); //coluna do array2
+
+            int res = 0;
+            int[,] matriz = new int[l1,c2];
+
+            for(int i = this.array1.GetLength(0)/2 ;i< l1;i++){
+                for (int j = 0; j < c2; j++){
+                    res = 0;
+                    for(int m =0;m<c1; m++)
+                    {
+                        res += this.array1[i,m] * this.array2[m, j];
+                    }
+                    System.Console.WriteLine($"Multiplicação resulta em : {res}");
+                    matriz[i,j] = res;
+                }
+            }          
+
+        }                                  
 
     }
 }
